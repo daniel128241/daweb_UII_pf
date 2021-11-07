17be0af36d8b7e5d8de8ed0f12eab255e28f6bda
daniel128241 <91646522+daniel128241@users.noreply.github.com>
2021-11-06 22:55:37 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,6 +4,6 @@
             <p> // SUBMÓDULO 2
                 Desarrolla aplicaciones Web con conexión a bases de datos</p>
             <p>https://github.com/daniel128241</p>
-            <center><img src="img/socialBar.png" alt="SB" height="40px" width="195px"></footer></center>
+            <center><img src="img/socialBar.png" alt="SB" height="40px" width="195px"></center>
 		</div>
 	</footer>
