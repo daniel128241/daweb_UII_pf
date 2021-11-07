00f6a806f582ac676348eea023f3bc40faf9f375
daniel128241 <daniel.zayles.04@gmail.com>
2021-11-06 22:39:07 -0700

    proyecto final

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+		<div class="wrapp">
+            <p> No lista 50 </p>
+            <p> // SUBMÓDULO 2
+                Desarrolla aplicaciones Web con conexión a bases de datos</p>
+            <p>https://github.com/elinava-web </p>
+            <center><img src="img/socialBar.png" alt="SB" height="40px" width="195px"></footer></center>
+		</div>
+	</footer>
