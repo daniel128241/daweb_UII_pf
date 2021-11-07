9a754ce4089a6d19c5b11ed9b99a9ee623fea4b2
daniel128241 <91646522+daniel128241@users.noreply.github.com>
2021-11-06 22:55:03 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
             <p> No lista 50 </p>
             <p> // SUBMÓDULO 2
                 Desarrolla aplicaciones Web con conexión a bases de datos</p>
-            <p>https://github.com/elinava-web </p>
+            <p>https://github.com/daniel128241</p>
             <center><img src="img/socialBar.png" alt="SB" height="40px" width="195px"></footer></center>
 		</div>
 	</footer>
